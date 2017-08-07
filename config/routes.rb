64c81037c874591b70069c034c6c7279f08d8db2
koukatou <koukatou@pasonatech.co.jp>
2017-08-07 05:27:12 +0000

    add helo_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+    get 'helo/index'
+    get 'helo', to: 'helo#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
