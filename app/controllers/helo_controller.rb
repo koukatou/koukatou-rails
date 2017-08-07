2f7b0a0206501ea03dea45d3aa696493419353d3
koukatou <koukatou@pasonatech.co.jp>
2017-08-07 06:43:53 +0000

    add index.html.erb

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,13 +1,18 @@
 class HeloController < ApplicationController
     def index
-        msg ='
-            <html>
-            <body>
-                <h1>Sample Page</h1>
-                <p>This is Rails Sample page!</p>
-            </body>
-            </html>
-            '
-        render html: msg.html_safe
+        # if params['msg'] != nil then
+        #     msg = 'Hello, ' + params['msg'] + '!'
+        # else
+        #     msg = 'this is Rails sample page.'
+        # end
+        # html ='
+        #     <html>
+        #     <body>
+        #         <h1>Sample Page</h1>
+        #         <p>' + msg + '</p>
+        #     </body>
+        #     </html>
+        #     '
+        # render html: html.html_safe
     end
 end
