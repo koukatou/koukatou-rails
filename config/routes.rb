8d685d58fe8ee061c947461d2604c91843256658
koukatou <koukatou@pasonatech.co.jp>
2017-08-08 04:40:14 +0000

    伝言板

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-    get 'helo/index'
-    get 'helo', to: 'helo#index'
-    get 'helo/other' #, to: 'helo#other'
-    post 'helo', to: 'helo#index'
-    post 'helo/index'
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
 end
