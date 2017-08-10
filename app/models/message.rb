1f628a4d711488ed0278e05c8bcdfd04e22120c7
koukatou <koukatou@pasonatech.co.jp>
2017-08-10 06:38:13 +0000

    アソシエーション

---
+++ app/models/message.rb
@@ -0,0 +1,6 @@
+class Message < ApplicationRecord
+    belongs_to :person
+    
+    validates :message, presence: {message:'を書いてください'}
+    
+end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ApplicationRecord
+    has_many :message
     
     validates :name, presence: { message: 'は必須項目です。'}
     validates :age, numericality: { message: 'は数字で入力してください。'}
+    validates :mail, email: {message: '正しいメールアドレスを入力してください'}
     
 end
