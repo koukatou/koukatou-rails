b9ddab63652c01841b2c49ca24b7ebc4ded893b1
koukatou <koukatou@pasonatech.co.jp>
2017-08-10 02:21:30 +0000

    バリデーション3

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ApplicationRecord
     
-    validates :name, presence: true
-    validates :age, numericality: true
+    validates :name, presence: { message: 'は必須項目です。'}
+    validates :age, numericality: { message: 'は数字で入力してください。'}
     
 end
